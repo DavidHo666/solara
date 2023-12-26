831bababa4b543f3091842d8c0b0cffd44def58b
Dawei He <hdw623@hotmail.com>
2023-12-26 15:36:53 -0500

    add timer.py using asyncio

--- timer.py
+++ timer.py
@@ -1,23 +1,42 @@
 import asyncio
 import solara
 
+elapsed_time=solara.reactive(0)
+duration=solara.reactive(60)
+@solara.component
+def timer_component(elapsed_time,duration, duration_callback, reset_callback):
+    progress_value = (elapsed_time.value / duration.value) * 100 if duration.value > 0 else 0
+    with solara.Card():
+        solara.ProgressLinear(value=progress_value,
+                              style={'color': 'blue',
+                                     'background-color': 'white'})
+        solara.Markdown(f'{elapsed_time}s')
+        solara.SliderInt(label='Duration', value=duration, min=0, max=100,
+                         on_value=lambda x: duration_callback(x))
+        solara.Button(label='Reset', on_click= reset_callback)
+
 @solara.component
 def Page():
-    elapsed_time, set_elapsed_time = solara.use_state(0.0)
-    duration, set_duration = solara.use_state(60.0)
-
+    timer_task=None
     async def timer_tick():
-        nonlocal elapsed_time
-        while elapsed_time < duration:
+        while elapsed_time.value < duration.value:
             await asyncio.sleep(1)
-            set_elapsed_time(elapsed_time + 1)
-            # elapsed_time+=1
+            new_val=elapsed_time.value+1
+            elapsed_time.set(new_val)
 
-    # Start the timer task
-    timer_task = asyncio.create_task(timer_tick())
+    def start_timer():
+        nonlocal timer_task
+        if timer_task:
+            timer_task.cancel()
+        timer_task = asyncio.create_task(timer_tick())
 
-    progress_value = (elapsed_time / duration) * 100 if duration > 0 else 0
-    with solara.Card():
-        solara.ProgressLinear(value=progress_value, style={'color': 'blue', 'background-color': 'white'})
-        solara.Markdown(f'{elapsed_time}s')
+    def on_duration_change(new_duration):
+        duration.set(new_duration)
+        start_timer()
 
+    def on_reset():
+        elapsed_time.set(0)
+        start_timer()
+
+    start_timer()
+    timer_component(elapsed_time,duration, on_duration_change, on_reset)
