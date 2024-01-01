6e1b42a583bd7e0d79a03ceda1abc3f02bfa990a
Dawei He <hdw623@hotmail.com>
2024-01-01 13:33:17 -0500

    update crud

--- crud.py
+++ crud.py
@@ -2,83 +2,54 @@ import solara
 
 @solara.component
 def Page():
-    # t_prefix, set_t_prefix = solara.use_state('')
     t_prefix=solara.use_reactive('')
-    print('t_prefix',t_prefix)
-    # t_name, set_t_name = solara.use_state('')
     t_name=solara.use_reactive('')
-    # t_surname, set_t_surname = solara.use_state('')
     t_surname=solara.use_reactive('')
-    name_values, set_name_values = solara.use_state([])
-    # name_values=solara.use_reactive([])
-    print('name_values2:',name_values)
-    # selected_name, set_selected_name = solara.use_state('')
+    # name_values, set_name_values = solara.use_state([])
+    name_values = solara.use_reactive([])
     selected_name=solara.use_reactive('')
     def filter_surname_prefix():
-        print('t_prefix: ', t_prefix.value)
         if not t_prefix.value:
-            print('not filtering')
-            return name_values
+            return name_values.value
         else:
-            print(f'filtering: {t_prefix}')
             prefix = t_prefix.value.lower()
-            return [name for name in name_values if name.split(',')[0].lower().startswith(prefix)]
+            return [name for name in name_values.value if name.split(',')[0].lower().startswith(prefix)]
     filtered_name_values = solara.use_memo(
         filter_surname_prefix,
-        [t_prefix.value, name_values]
+        [t_prefix.value, name_values.value]
     )
-
-
     print("name_values: ", name_values)
     print("filtered_name_values: ", filtered_name_values)
 
-    # def filter_surname_prefix(prefix):
-    #     if not prefix:
-    #         set_filtered_name_values(name_values)
-    #     else:
-    #         prefix = prefix.lower()
-    #         set_filtered_name_values([name for name in name_values if name.split(',')[0].lower().startswith(prefix)])
-    #         set_t_prefix(prefix)
-
     def create_name():
         new_name = t_surname.value + ','+ t_name.value
-        print("calling set_name_values")
-        # name_values.append(new_name)
-        # set_name_values(name_values+[new_name])
-        name_values.append(new_name)
-        print("name_values1: ", name_values)
-        print('filtered_name_values1', filtered_name_values)
-
-        # set_t_name('')
-        t_name.value=''
-        t_surname.value=''
-
+        name_values.set(name_values.value + [new_name])
+        # name_values.value.append(new_name)
+        t_name.set('')
+        t_surname.set('')
 
     def update_name():
-        idx=name_values.index(selected_name.value)
+        idx=name_values.value.index(selected_name.value)
         new_name = t_surname.value + ','+ t_name.value
-        name_values[idx] = new_name
-        # set_t_name('')
-        t_name.value = ''
-        t_surname.value=''
+        name_values.set(name_values.value[:idx] + [new_name] + name_values.value[idx+1:])
+        # name_values[idx] = new_name
+        t_name.set('')
+        t_surname.set('')
 
     def delete_name():
-        name_values.remove(selected_name.value)
-        # set_selected_name('')
-        selected_name.value=''
-        # set_t_name('')
-        t_name.value = ''
-        t_surname.value=''
-
-
-
+        idx=name_values.value.index(selected_name.value)
+        name_values.set(name_values.value[:idx] + name_values.value[idx+1:])
+        selected_name.set('')
+        t_name.set('')
+        t_surname.set('')
 
     with solara.Card():
         with solara.Columns([1,1]):
             with solara.Card():
                 solara.InputText(label='Filter prefix', value=t_prefix,
                                  continuous_update=True)
-                solara.Select(label='List of names', value=selected_name, values=filtered_name_values)
+                solara.Select(label='List of names', value=selected_name,
+                              values=filtered_name_values)
                 solara.Button(label='Create', on_click=create_name)
                 solara.Button(label='Update', on_click=update_name)
                 solara.Button(label='Delete', on_click=delete_name)
